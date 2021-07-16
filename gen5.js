256b67d94827d2e80d733068e89f51cd66de42ba
idndot <87428185+idndot@users.noreply.github.com>
2021-07-16 16:06:10 +0700

    Update and rename gen5.js to gen6.js

--- gen5.js
+++
@@ -1,3 +0,0 @@
-loadyn("https://cdn.statically.io/gh/idndot/s/main/css/gen2.css", "css")
-loadyn("https://cdn.statically.io/gh/idndot/s/main/js/gen1.js", "js")
-loadyn("https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js", "js")

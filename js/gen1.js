6f7a5c89c3a2e06afd1a43b268031cb39aa4c54c
idndot <87428185+idndot@users.noreply.github.com>
2021-07-16 15:07:51 +0700

    Update gen1.js

--- js/gen1.js
+++ js/gen1.js
@@ -1,1 +1,2 @@
-
+/* Loader */
+document.body.insertAdjacentHTML("beforeend",'<div id="load_page" class="load_hide"><div class="load_er load_spin"></div></div>'),window.addEventListener("beforeunload",function(){const d=document.getElementById("load_page");setTimeout(function(){d.style.display="block"},1),setTimeout(function(){d.classList.remove("load_hide"),d.classList.add("load_show")},200)});

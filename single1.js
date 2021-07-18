40bb49b77567f1193e3d3673505110a00dbe7732
idndot <87428185+idndot@users.noreply.github.com>
2021-07-18 08:46:07 +0700

    Update and rename single1.js to single2.js

--- single1.js
+++
@@ -1,2 +0,0 @@
-loadyn("https://cdn.statically.io/gh/idndot/s/main/js/single1.js", "js")
-loadyn("https://cdn.jsdelivr.net/gh/Indzign/theme@master/highlightprox.js", "js")
---
+++ single2.js
@@ -0,0 +1,2 @@
+loadyn("https://cdn.statically.io/gh/idndot/s/main/js/single1.js","js")
+loadyn("https://cdn.jsdelivr.net/gh/Indzign/theme@master/highlightprox.js","js")

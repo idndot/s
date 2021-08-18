c36c18198550e4d5ea4077f1235b5a9d1c6fa320
idndot <87428185+idndot@users.noreply.github.com>
2021-08-18 23:45:01 +0700

    Update and rename post2.js to post3.js

--- post3.js
+++ post3.js
@@ -1,2 +1,2 @@
 loadyn("https://cdn.statically.io/gh/idndot/s/main/css/post2.min.css","css")
-loadyn("https://cdn.statically.io/gh/idndot/s/main/js/post2.min.js","js")
+loadyn("https://cdn.statically.io/gh/idndot/s/main/js/post3.min.js","js")

64db3b97d6c1a3d1915679db6c7559fd8d7f1d52
idndot <87428185+idndot@users.noreply.github.com>
2021-08-21 22:51:49 +0700

    Update and rename post_music2.js to post_music3.js

--- post_music2.js
+++
@@ -1,2 +0,0 @@
-loadyn("https://cdn.statically.io/gh/idndot/s/main/css/post_music1.min.css","js")
-loadyn("https://cdn.statically.io/gh/idndot/s/main/js/post_music1.min.js","js")
---
+++ post_music3.js
@@ -0,0 +1,2 @@
+loadyn("https://cdn.statically.io/gh/idndot/s/main/css/post_music1.min.css","css")
+loadyn("https://cdn.statically.io/gh/idndot/s/main/js/post_music1.min.js","js")
